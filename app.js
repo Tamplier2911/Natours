c82abbb270afa0c228dff67223e7120880868c60
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2019-11-06 16:43:39 +0200

    Installed helmet package, implemented secure http headers. Limited body payload in body parser.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const morgan = require('morgan');
 
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,10 +12,16 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // GLOBAL MIDDLEWARES
+
+// security http headers
+app.use(helmet());
+
+// dev logger feature
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// rate-limiter
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,9 +30,13 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser (read data from body into req.body)
+app.use(express.json({ limit: '10kb' }));
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -37,15 +48,6 @@ app.use('/api/v1/users', userRouter);
 
 // HANDLING UNHANDLED ROUTES
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} route.`
-  // });
-
-  // const err = new Error(`Can't find ${req.originalUrl} route on this server.`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-
   const error = new AppError(
     `Can't find ${req.originalUrl} route on this server.`,
     404
