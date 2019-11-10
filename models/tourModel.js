1ac245f9d279bde5237f831f386fae9fbac27d60
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2019-11-11 00:17:23 +0200

    Continue to modeling data, exploring embeding and referensing, taking use of child referencing and populations of referenced fields.

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
 
+// const User = require('./userModel');
+
 // SCHEMAS
 const tourSchema = new mongoose.Schema(
   {
@@ -109,6 +111,13 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
+    ],
+    // guides: Array
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
     ]
   },
   {
@@ -129,6 +138,13 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
+// IF WE ADDED GUIDES BY EMBEDING
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
 // tourSchema.post('save', function(doc, next) {
 //   console.log('First chained post-save-hook middleware.');
 //   next();
@@ -143,6 +159,14 @@ tourSchema.pre('save', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
   // this.start = Date.now();
+  next();
+});
+
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
   next();
 });
 
